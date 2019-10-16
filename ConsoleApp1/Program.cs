c1e385e41a9cf0edfa556724c43b53740b028527
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 12:42:00 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace esercizio_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserisci la temperatura: ");
+            double temperatura = double.parse(Console.readLine());
+            if (temperatura>18)
+            {
+                Console.ReadLine($"la temperatura della serra è stabile");
+            }
+            if(temperatura>5)
+            {
+                Console.ReadLine($"la temperatura della serra è in situazione di pericolo");
+            }
+            else if (temperatura < 18)
+            {
+                Console.ReadLine($"la temperatura della serra è in situazione di pericolo");
+            }
+            if(temperatura<5)
+            {
+                Console.ReadLine($"la temperatura della serra sta facendo danni irreparabili");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Esercizio_5()/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_5__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserisci la temperatura: ");
+            double temperatura = double.Parse(Console.ReadLine());
+            if (temperatura >= 18)
+            {
+                Console.WriteLine($"la temperatura della serra è stabile");
+            }
+            if (temperatura > 5)
+            {
+                Console.WriteLine($"la temperatura della serra è in situazione di pericolo");
+            }
+            else if (temperatura < 18)
+            {
+                Console.WriteLine($"la temperatura della serra è in situazione di pericolo");
+            }
+            if (temperatura <= 5)
+            {
+                Console.WriteLine($"la temperatura della serra sta facendo danni irreparabili");
+            }
+            Console.ReadLine();
+        }
+    }
+}
